af7152c5c009ca3a99f5ef7cde5e0ee3b3430f24
Sam Esgate <sam.esgate@sparx.co.uk>
2019-05-09 20:55:55 +0100

    class -> className

--- src/messages.js
+++ src/messages.js
@@ -20,33 +20,33 @@ export class Messages extends React.Component {
 
     render() {
         return (
-            <div class="messages">
-                <div class="message"><div class="text">Arranging Algebra</div></div>
-                <div class="message"><div class="text">Baking Bar Charts</div></div>
-                <div class="message"><div class="text">Collecting Calculus</div></div>
-                <div class="message"><div class="text">Defining Decimals</div></div>
-                <div class="message"><div class="text">Extracting Euler</div></div>
-                <div class="message"><div class="text">Finding Fractions</div></div>
-                <div class="message"><div class="text">Generating Graphs</div></div>
-                <div class="message"><div class="text">Hunting Hypotenuse</div></div>
-                <div class="message"><div class="text">Integrating Infinity</div></div>
-                <div class="message"><div class="text">Juggling ???</div></div>
-                <div class="message"><div class="text">Knitting Kites</div></div>
-                <div class="message"><div class="text">Linking Lines</div></div>
-                <div class="message"><div class="text">Mastering Multiplication</div></div>
-                <div class="message"><div class="text">Noticing Normals</div></div>
-                <div class="message"><div class="text">Organising Outliers</div></div>
-                <div class="message"><div class="text">Perfecting Percentages</div></div>
-                <div class="message"><div class="text">Querying Quadrants</div></div>
-                <div class="message"><div class="text">Rescuing Radii</div></div>
-                <div class="message"><div class="text">Storing Squares</div></div>
-                <div class="message"><div class="text">Targetting Times Tables</div></div>
-                <div class="message"><div class="text">Understanding Units</div></div>
-                <div class="message"><div class="text">Vindicating Vertices</div></div>
-                <div class="message"><div class="text">Welcoming Weights</div></div>
-                <div class="message"><div class="text">X-Raying X</div></div>
-                <div class="message"><div class="text">Yanking Years</div></div>
-                <div class="message"><div class="text">Zesting Zero</div></div>
+            <div className="messages">
+                <div className="message"><div className="text">Arranging Algebra</div></div>
+                <div className="message"><div className="text">Baking Bar Charts</div></div>
+                <div className="message"><div className="text">Collecting Calculus</div></div>
+                <div className="message"><div className="text">Defining Decimals</div></div>
+                <div className="message"><div className="text">Extracting Euler</div></div>
+                <div className="message"><div className="text">Finding Fractions</div></div>
+                <div className="message"><div className="text">Generating Graphs</div></div>
+                <div className="message"><div className="text">Hunting Hypotenuse</div></div>
+                <div className="message"><div className="text">Integrating Infinity</div></div>
+                <div className="message"><div className="text">Juggling ???</div></div>
+                <div className="message"><div className="text">Knitting Kites</div></div>
+                <div className="message"><div className="text">Linking Lines</div></div>
+                <div className="message"><div className="text">Mastering Multiplication</div></div>
+                <div className="message"><div className="text">Noticing Normals</div></div>
+                <div className="message"><div className="text">Organising Outliers</div></div>
+                <div className="message"><div className="text">Perfecting Percentages</div></div>
+                <div className="message"><div className="text">Querying Quadrants</div></div>
+                <div className="message"><div className="text">Rescuing Radii</div></div>
+                <div className="message"><div className="text">Storing Squares</div></div>
+                <div className="message"><div className="text">Targetting Times Tables</div></div>
+                <div className="message"><div className="text">Understanding Units</div></div>
+                <div className="message"><div className="text">Vindicating Vertices</div></div>
+                <div className="message"><div className="text">Welcoming Weights</div></div>
+                <div className="message"><div className="text">X-Raying X</div></div>
+                <div className="message"><div className="text">Yanking Years</div></div>
+                <div className="message"><div className="text">Zesting Zero</div></div>
             </div>
         );
     }
