8f1af7ef551de46b95ca2648b4d51f466ef2701c
Sam Esgate <sam.esgate@sparx.co.uk>
2019-05-11 11:07:18 +0100

    Shaking

--- src/simple_x.js
+++ src/simple_x.js
@@ -3,19 +3,21 @@ import anime from 'animejs';
 
 export class SimpleX extends React.Component {
     componentDidMount() {
-        const animationTime = 4000;
+        const fillTime = 4000;
+        const shakeTime = 1000;
+        const sparkTime = 300;
+        const endTime = 1000;
 
         const timeline = anime.timeline({
             loop: true,
-            endDelay: 1000,
+            endDelay: endTime,
         });
 
         // Up-Down
         timeline.add({
             targets: '#water',
             translateY: [480+60, 0],
-            // delay: 5000,
-            duration: animationTime,
+            duration: fillTime,
             easing: 'linear',
             direction: 'alternate',
         }, 0);
@@ -36,11 +38,93 @@ export class SimpleX extends React.Component {
         timeline.add({
             targets: '#water',
             keyframes,
-            duration: animationTime,
+            duration: fillTime,
             easing: 'easeInOutSine',
             direction: 'alternate',
             loop: 3,
         }, 0);
+
+        // Shake
+        timeline.add({
+            targets: ['#sparxXOutline', '#sparxX', '#water'],
+            translateX: [
+                { value: 5 },
+                { value: -5 },
+                { value: 5 },
+                { value: -5 },
+                { value: -5 },
+                { value: 5 },
+                { value: -5 },
+                { value: 5 },
+                { value: -5 },
+                { value: -5 },
+                { value: 5 },
+                { value: 0 },
+                { value: 5 },
+                { value: 5 },
+                { value: 0 },
+                { value: -5 },
+                { value: 0 },
+                { value: 5 },
+                { value: -5 },
+                { value: 5 },
+                { value: -5 },
+                { value: -5 },
+                { value: 5 },
+                { value: -5 },
+                { value: 5 },
+                { value: 0 },
+                { value: 5 },
+                { value: 0 },
+                { value: -5 },
+                { value: 0 },
+            ],
+            translateY: [
+                { value: 0 },
+                { value: 0 },
+                { value: -5 },
+                { value: 5 },
+                { value: -5 },
+                { value: 5 },
+                { value: -5 },
+                { value: -5 },
+                { value: 5 },
+                { value: 0 },
+                { value: -5 },
+                { value: 0 },
+                { value: -5 },
+                { value: 0 },
+                { value: 0 },
+                { value: 0 },
+                { value: 0 },
+                { value: -5 },
+                { value: 5 },
+                { value: -5 },
+                { value: 5 },
+                { value: -5 },
+                { value: -5 },
+                { value: 5 },
+                { value: 0 },
+                { value: -5 },
+                { value: 0 },
+                { value: -5 },
+                { value: 0 },
+                { value: 0 },
+            ],
+            // scale: [
+            //     { value: 1 },
+            //     { value: 0.8 },
+            //     { value: 1.05 },
+            // ],
+            duration: shakeTime,
+        }, fillTime)
+
+        // // Grow
+        // timeline.add({
+        //     targets: ['#sparxXOutline', '#sparxX', '#water'],
+        //     scale: [1, 1.05],
+        //     duration: shakeTime,
+        // }, fillTime)
 
         // Background Colour(s)
         timeline.add({
@@ -48,22 +132,22 @@ export class SimpleX extends React.Component {
             background: '#00b1dc',
             easing: 'easeInOutSine',
             duration: 200,
-        }, animationTime)
+        }, fillTime + shakeTime)
         timeline.add({
             targets: '#sparxX',
             fill: ['#94b3ba', '#00b1dc'],
             easing: 'easeInOutSine',
             duration: 200,
-        }, animationTime)
+        }, fillTime + shakeTime)
 
         // Animate Sparks
         timeline.add({
           targets: '.spark',
           strokeDashoffset: [anime.setDashoffset, 0],
           easing: 'easeInOutCubic',
-          delay: function(el, i) { return i * 5 },
+          // delay: function(el, i) { return i * 5 },
           duration: 400,
-        }, animationTime);
+        }, fillTime + shakeTime);
 
         anime(timeline);
     }
@@ -73,17 +157,14 @@ export class SimpleX extends React.Component {
             <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="640" height="640">
                 <defs>
                     <path d="M0 640L640 640L640 0C536.19 20.08 429.52 20.08 320 0C210.48 -20.08 103.81 -20.08 0 0L0 640Z" id="water"/>
-                    <path d="M0 0L640 0L640 640L0 640L0 640L0 0ZM21.43 598.96L225.35 598.32L320.59 440.27L416.43 598.96L618.57 598.96L432.65 300.3L593.46 41.04L400.21 41.04L320.59 172.23L241.28 41.04L37.72 41.04L213.18 300.3L213.18 300.3L21.43 598.96Z" id="sparxX"/>
+                    <path d="M-20 -20L660 -20L660 660L-20 660L-20 660L-20 -20ZM21.43 598.96L225.35 598.32L320.59 440.27L416.43 598.96L618.57 598.96L432.65 300.3L593.46 41.04L400.21 41.04L320.59 172.23L241.28 41.04L37.72 41.04L213.18 300.3L213.18 300.3L21.43 598.96Z" id="sparxX"/>
                     <path d="M21.43 598.96L225.35 598.32L320.59 440.27L416.43 598.96L618.57 598.96L432.65 300.3L593.46 41.04L400.21 41.04L320.59 172.23L241.28 41.04L37.72 41.04L213.18 300.3L213.18 300.3L21.43 598.96Z" id="sparxXOutline"/>
                     <path id="spark" d="M244.58 242.49L212.76 300.95" opacity="1" fillOpacity="0" stroke="#222f32" strokeWidth="5" strokeOpacity="1"/>
                 </defs>
                 <g>
                     <g>
                         <g>
-                            <use xlinkHref="#water" opacity="1" fill="#00b7e0" fillOpacity="1"/>
-                            <g>
-                                <use xlinkHref="#water" opacity="1" fillOpacity="0" stroke="#000000" strokeWidth="3" strokeOpacity="1"/>
-                            </g>
+                            <use xlinkHref="#water" fill="#00b7e0" opacity="1" fillOpacity="1" stroke="#000000" strokeWidth="3" strokeOpacity="1"/>
                         </g>
                         <g>
                             <use xlinkHref="#sparxX" opacity="1" fill="#94b3ba" fillOpacity="1"/>
